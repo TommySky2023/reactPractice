1c060123b3144913cc1e2dc79fc1e4d1c256a223
Shibata0806 <marsonperson@gmail.com>
2024-08-12 17:50:26 +0900

    Day6 カスタムフック

--- react-practice/src/js/components/todo/NewTodoForm.tsx
+++ react-practice/src/js/components/todo/NewTodoForm.tsx
@@ -1,29 +1,19 @@
 import * as React from "react";
 import { TextField } from "../parts/TextField";
 import { useState } from "react";
-import { Todo } from "./type";
 import { Button } from "../parts/Button";
 
 type Props = {
-  setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>;
+  addTodo: (newTask: string, newPerson: string, newDeadline: string) => void;
 };
 
-export const NewTodoForm = ({ setTodoList }: Props) => {
+export const NewTodoForm = ({ addTodo }: Props) => {
   const [newTask, setNewTask] = useState<string>("");
   const [newPerson, setNewPerson] = useState<string>("");
   const [newDeadline, setNewDeadline] = useState<string>("");
 
   const addNewTodo = () => {
-    setTodoList((prev: Todo[]) => [
-      ...prev,
-      {
-        id: Date.now(),
-        task: newTask,
-        person: newPerson,
-        deadline: newDeadline,
-      },
-    ]);
-
+    addTodo(newTask, newPerson, newDeadline);
     setNewTask("");
     setNewPerson("");
     setNewDeadline("");
--- react-practice/src/js/components/todo/TodoItem.tsx
+++ react-practice/src/js/components/todo/TodoItem.tsx
@@ -1,31 +1,22 @@
 import * as React from "react";
-import { Todo } from "./type";
 import { Button } from "../parts/Button";
-type TodoItemProps = {
+
+type Props = {
   id: number;
   task: string;
   person: string;
   deadline: string;
-  setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>;
+  deleteTodo: (id: number) => void;
 };
 
-export const TodoItem: React.FC<TodoItemProps> = ({
-  id,
-  task,
-  person,
-  deadline,
-  setTodoList,
-}) => {
-  const deleteTodo = () =>
-    setTodoList((prev) => prev.filter((todo) => todo.id !== id));
-
+export const TodoItem = ({ id, task, person, deadline, deleteTodo }: Props) => {
   return (
     <li className="grid grid-cols-4">
       <div>{task}</div>
       <div>{person}</div>
       <div>{deadline}</div>
       <div>
-        <Button onClick={deleteTodo} color="red">
+        <Button onClick={() => deleteTodo(id)} color="red">
           削除
         </Button>
       </div>
